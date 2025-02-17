ea0e4efe674423923a3ac0339eb9dd0aaea01598
Andraž Starc <andraz.starc@ibe.si>
2025-02-17 17:27:50 +0100

    Add Procfile and initial setup

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const session = require('cookie-session');
-const { PORT, SERVER_SESSION_SECRET } = require('./config.js');
+const { PORT, SERVER_SESSION_SECRET, APS_CALLBACK_URL } = require('./config.js');
 
 let app = express();
+
+// Preveri, ali se vrednost APS_CALLBACK_URL pravilno bere iz .env datoteke
+console.log('APS_CALLBACK_URL:', APS_CALLBACK_URL); // To bo izpisalo vrednost URL-ja iz .env datoteke
+
 app.use(express.static('wwwroot'));
 app.use(session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 }));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/hubs.js'));
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
